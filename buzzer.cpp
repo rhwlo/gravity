25b338e95b6c38497f9d942860f020b65e8b93f3
Joshu Coats <joshu@fearchar.net>
2024-08-01 15:11:15 -0700

    you call that an e flat? this is an e flat

--- buzzer.cpp
+++ buzzer.cpp
@@ -2,21 +2,39 @@
 #include "buzzer.h"
 
 void singleBeep(void) {
-    tone(BUZZER_PIN, 662);
+    tone(BUZZER_PIN, BUZZER_TONE);
+    delay(110);
+    noTone(BUZZER_PIN);
+}
+
+void doubleBeep(void) {
+    tone(BUZZER_PIN, BUZZER_TONE);
+    delay(110);
+    noTone(BUZZER_PIN);
+    delay(40);
+    tone(BUZZER_PIN, BUZZER_TONE);
+    delay(110);
+    noTone(BUZZER_PIN);
+}
+
+void chirp(void) {
+    tone(BUZZER_PIN, BUZZER_TONE);
+    delay(60);
+    tone(BUZZER_PIN, BUZZER_TONE_TWO);
     delay(110);
     noTone(BUZZER_PIN);
 }
 
 void tripleBeep(void) {
-    tone(BUZZER_PIN, 662);
+    tone(BUZZER_PIN, BUZZER_TONE);
     delay(110);
     noTone(BUZZER_PIN);
     delay(40);
-    tone(BUZZER_PIN, 662);
+    tone(BUZZER_PIN, BUZZER_TONE);
     delay(110);
     noTone(BUZZER_PIN);
     delay(40);
-    tone(BUZZER_PIN, 662);
+    tone(BUZZER_PIN, BUZZER_TONE);
     delay(110);
     noTone(BUZZER_PIN);
 }
--- buzzer.h
+++ buzzer.h
@@ -2,8 +2,10 @@
 #define BUZZER_H
 
 #define BUZZER_PIN              9
-#define BUZZER_TONE             662  // Eb
+#define BUZZER_TONE             622  // Eb
+#define BUZZER_TONE_TWO         932  // Eb
 void singleBeep(void);
+void doubleBeep(void);
 void tripleBeep(void);
 
 #endif
